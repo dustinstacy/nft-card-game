4320ae6c01d7d0759b01a52c0ca014afb62e0631
Dustin Stacy <dustin.stacy@outlook.com>
2022-12-29 11:49:12 -0500

    Add start of Smart Contract Interaction

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useGlobalContext } from '../context';
 
 import { PageHOC } from '../components';
+import { useNavigate } from 'react-router-dom';
 
 const CreateBattle = () => {
+  const { contract, walletAddress } = useGlobalContext();
+  const navigate = useNavigate();
+
+useEffect(() => {
+    const createPlayerToken = async () => {
+      const playerExists = await contract.isPlayer(walletAddress);
+      const playerTokenExists = await contract.isPlayerToken(walletAddress);
+
+      console.log({
+        playerExists,
+        playerTokenExists
+      })
+
+      if (!playerExists && !playerTokenExists) navigate('/');
+    };
+
+    if (contract) createPlayerToken();
+  }, [walletAddress])
+
   return <div />
 };
 
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,27 +1,73 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { useGlobalContext } from '../context';
 import { PageHOC, CustomInput, CustomButton } from '../components';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const { contract, walletAddress } = useGlobalContext();
+  const { contract, walletAddress, setShowAlert } = useGlobalContext();
   const [playerName, setPlayerName] = useState('');
 
-  const handleClick = async () => { };
+  const navigate = useNavigate();
 
-  return <div className='flex flex-col'>
-    <CustomInput
-      label="Name"
-      placeholder="Enter your player name"
-      value={playerName}
-      handleValueChange={setPlayerName}
-    />
-    <CustomButton
-      title="Register"
-      handleClick={handleClick}
-      restStyles='mt-6'
-    />
-  </div>
+  const handleClick = async () => {
+    try {
+      const playerExists = await contract.isPlayer(walletAddress);
+
+      if (!playerExists) {
+        await contract.registerPlayer(playerName, playerName)
+
+        setShowAlert({
+          status: true,
+          type: 'info',
+          message: `${playerName} is being summoned!`
+        });
+
+        setTimeout(() => navigate('/create-battle'), 6000)
+      }
+    } catch (error) {
+              setShowAlert({
+          status: true,
+          type: 'failure',
+          message: "Something went wrong!",
+        })
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    const createPlayerToken = async () => {
+      const playerExists = await contract.isPlayer(walletAddress);
+      const playerTokenExists = await contract.isPlayerToken(walletAddress);
+
+      console.log({
+        playerExists,
+        playerTokenExists
+      })
+
+      if (playerExists && playerTokenExists) navigate('/create-battle');
+    };
+
+    if (contract) createPlayerToken();
+  }, [walletAddress])
+
+  return (
+    walletAddress && (
+      <div className='flex flex-col'>
+        <CustomInput
+          label="Name"
+          placeholder="Enter your player name"
+          value={playerName}
+          handleValueChange={setPlayerName}
+        />
+        <CustomButton
+          title="Register"
+          handleClick={handleClick}
+          restStyles='mt-6'
+        />
+      </div>
+    )
+  );
 };
 
 export default PageHOC(
